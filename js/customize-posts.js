46d92f0f1b3809f9661ae50a02baf92d404cfa89
Weston Ruter <weston@x-team.com>
2014-05-03 00:15:54 -0700

    Expand Posts section and move to top if previewing post

--- js/customize-posts.js
+++ js/customize-posts.js
@@ -1,5 +1,7 @@
 /*global jQuery, wp */
 wp.customize.bind( 'ready', function () {
-	// @todo If previewing a post, load it up here, show the section, and scroll into view
-	jQuery( '#accordion-section-posts' ).addClass( 'open' );
+
+	// Auto-open the posts section if it is at the top (we're previewing a post)
+	jQuery( '#accordion-section-posts.top' ).addClass( 'open' );
+
 } );
--- php/class-wp-customize-posts.php
+++ php/class-wp-customize-posts.php
@@ -29,14 +29,21 @@ final class WP_Customize_Posts {
 
 		$section_id = 'posts';
 
+		$previewed_post = $this->get_previewed_post();
+		if ( $previewed_post && ! $this->current_user_can_edit_post( $previewed_post ) ) {
+			$previewed_post = null;
+		}
+
 		$this->manager->add_section( $section_id, array(
-			'title'    => __( 'Posts' ),
-			'priority' => 900, // @todo If the request is for a preview of a post, change this to top priority
+			'title'      => __( 'Posts' ),
+			'priority'   => $previewed_post ? 1 : 900,
+			'capability' => 'edit_posts',
 		) );
 
 		$this->manager->add_setting( 'post_selection', array(
+			'default'    => $previewed_post ? $previewed_post->ID : 0,
 			'type'       => 'custom',
-			'capability' => 'edit_posts',
+			'capability' => $previewed_post ? get_post_type_object( $previewed_post->post_type )->cap->edit_posts : 'edit_posts',
 		) );
 
 		$control = new WP_Post_Selector_Customize_Control( $this->manager, 'post_selection', array(
@@ -45,6 +52,30 @@ final class WP_Customize_Posts {
 		$this->manager->add_control( $control );
 
 		add_action( 'customize_controls_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+	}
+
+	/**
+	 * @return WP_Post|null
+	 */
+	public function get_previewed_post() {
+		if ( empty( $_GET['url'] ) ) {
+			return null;
+		}
+		$previewed_url = wp_unslash( $_GET['url'] );
+		$post_id = url_to_postid( $previewed_url );
+		if ( 0 === $post_id ) {
+			return null;
+		}
+		$post = get_post( $post_id );
+		return $post;
+	}
+
+	/**
+	 * @param WP_Post $post
+	 * @return boolean
+	 */
+	public function current_user_can_edit_post( WP_Post $post ) {
+		return current_user_can( get_post_type_object( $post->post_type )->cap->edit_post, $post->ID );
 	}
 
 	/**
