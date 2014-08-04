8dd1b5046be59bc6611487c8072eb9622b68f066
Weston Ruter <weston@x-team.com>
2014-08-03 22:22:00 -0700

    Version bump 0.2

--- customize-posts.php
+++ customize-posts.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Customize Posts
  * Description: Manage posts and postmeta via the customizer.
- * Version: 0.1
+ * Version: 0.2
  * Author: X-Team
  * Author URI: http://x-team.com/wordpress/
  * License: GPLv2+
