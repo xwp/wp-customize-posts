1f570479dbb2257a8f3598c839e136619c2ca88d
Weston Ruter <weston@x-team.com>
2014-09-17 00:59:20 -0700

    Add support for customize cap
    Fixes #18

--- customize-posts.php
+++ customize-posts.php
@@ -37,3 +37,25 @@ function wp_customize_posts_init() {
 	$wp_customize->posts = new WP_Customize_Posts( $wp_customize );
 }
 add_action( 'customize_register', 'wp_customize_posts_init' );
+
+
+/**
+ * Let users who can edit posts also access the Customizer because there is something for them there.
+ *
+ * @todo Add Customize link to admin bar, when editing a post, and perhaps in the admin menu
+ *
+ * @see https://core.trac.wordpress.org/ticket/28605
+ * @param array $allcaps
+ * @param array $caps
+ * @param array $args
+ *
+ * @return array
+ */
+function wp_customize_posts_grant_capability( $allcaps, $caps, $args ) {
+	if ( ! empty( $allcaps['edit_posts'] ) && ! empty( $args ) && 'customize' === $args[0] ) {
+		$allcaps = array_merge( $allcaps, array_fill_keys( $caps, true ) );
+	}
+	return $allcaps;
+}
+
+add_filter( 'user_has_cap', 'wp_customize_posts_grant_capability', 10, 3 );
