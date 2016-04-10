24002c7d6a0444619e51b491109a415f3db7f6d3
Weston Ruter <weston@xwp.co>
2016-04-11 09:45:51 +1000

    Check if get_current_screen() returns object before accessing property

--- php/class-edit-post-preview.php
+++ php/class-edit-post-preview.php
@@ -75,7 +75,7 @@ class Edit_Post_Preview {
 	 * @return WP_Post|null
 	 */
 	public function get_previewed_post() {
-		if ( function_exists( 'get_current_screen' ) && 'post' === get_current_screen()->base ) {
+		if ( function_exists( 'get_current_screen' ) && get_current_screen() && 'post' === get_current_screen()->base ) {
 			$post = get_post();
 		} elseif ( is_preview() ) {
 			$post = get_post();
@@ -97,7 +97,7 @@ class Edit_Post_Preview {
 	 * Enqueue scripts for post edit screen.
 	 */
 	public function enqueue_admin_scripts() {
-		if ( ! function_exists( 'get_current_screen' ) || 'post' !== get_current_screen()->base ) {
+		if ( ! function_exists( 'get_current_screen' ) || ! get_current_screen() || 'post' !== get_current_screen()->base ) {
 			return;
 		}
 		wp_enqueue_script( 'edit-post-preview-admin' );
