dafec6ba48fd38367f508008a557e9c9bec5c981
Weston Ruter <weston@xwp.co>
2016-04-19 13:06:11 -0700

    Add missing class-wp-customize-post-discussion-fields-control.php

---
+++ php/class-wp-customize-post-discussion-fields-control.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Customize Dynamic Control Class
+ *
+ * @package WordPress
+ * @subpackage Customize
+ */
+
+/**
+ * Class WP_Customize_Post_Discussion_Fields_Control
+ */
+class WP_Customize_Post_Discussion_Fields_Control extends WP_Customize_Dynamic_Control {
+
+	/**
+	 * Type of control, used by JS.
+	 *
+	 * @access public
+	 * @var string
+	 */
+	public $type = 'post_discussion_fields';
+
+	/**
+	 * Render the Underscore template for this control.
+	 *
+	 * @access protected
+	 * @codeCoverageIgnore
+	 */
+	protected function content_template() {
+		$data = $this->json();
+		?>
+		<#
+		_.defaults( data, <?php echo wp_json_encode( $data ) ?> );
+		data.ping_status_input_id = 'input-' + String( Math.random() );
+		data.comment_status_input_id = 'input-' + String( Math.random() );
+		#>
+
+		<span class="customize-control-title"><label for="{{ data.input_id }}">{{ data.label }}</label></span>
+		<# if ( data.description ) { #>
+			<span class="description customize-control-description">{{ data.description }}</span>
+		<# } #>
+
+		<# if ( data.post_type_supports.comments ) { #>
+			<p>
+			<label for="{{ data.comment_status_input_id }}">
+				<input
+					id="{{ data.comment_status_input_id }}"
+					type="checkbox"
+					<# _.each( data.input_attrs, function( value, key ) { #>
+						{{{ key }}}="{{ value }}"
+					<# } ) #>
+					data-on-value="open"
+					data-off-value="closed"
+					data-customize-setting-property-link="comment_status"
+				/>
+				<?php esc_html_e( 'Allow comments.', 'customize-posts' ) ?>
+			</label>
+			</p>
+		<# } #>
+		<# if ( data.post_type_supports.trackbacks ) { #>
+			<p>
+			<label for="{{ data.ping_status_input_id }}">
+				<input
+					id="{{ data.ping_status_input_id }}"
+					type="checkbox"
+					<# _.each( data.input_attrs, function( value, key ) { #>
+						{{{ key }}}="{{ value }}"
+					<# } ) #>
+					data-on-value="open"
+					data-off-value="closed"
+					data-customize-setting-property-link="ping_status"
+				/>
+				<?php
+					echo sprintf(
+						/* translators: %s: Codex URL */
+						__( 'Allow <a href="%s">trackbacks and pingbacks</a> on this page.', 'customize-posts' ),
+						'https://codex.wordpress.org/Introduction_to_Blogging#Managing_Comments'
+					); // WPCS: xss ok.
+				?>
+			</label>
+			</p>
+		<# } #>
+
+		<div class="customize-setting-validation-message error" aria-live="assertive"></div>
+		<?php
+	}
+}
