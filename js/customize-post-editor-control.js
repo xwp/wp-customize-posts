e4b2c4456300bb4881a1c91078b819318a0f31b7
Weston Ruter <weston@xwp.co>
2016-08-18 11:18:58 -0700

    Remove bound events when editor control is removed; eliminate anonymous functions

--- js/customize-post-editor-control.js
+++ js/customize-post-editor-control.js
@@ -17,6 +17,19 @@
 		initialize: function initialize( id, options ) {
 			var control = this, args;
 
+			_.bindAll(
+				control,
+				'handleRemoval',
+				'onTextEditorChange',
+				'onVisualEditorChange',
+				'onMousedownEditorDragbar',
+				'onMouseupEditorDragbar',
+				'resizeEditorWithWindowResize',
+				'updateEditorContentsOnSettingChange',
+				'handleSectionExpansionToggled',
+				'handleToggleEditorButtonClick'
+			);
+
 			args = {};
 			args.params = _.extend(
 				{
@@ -44,6 +57,41 @@
 			control.deferred.embedded.done( function() {
 				control.initEditor();
 			});
+
+			api.control.bind( 'remove', control.handleRemoval );
+		},
+
+		/**
+		 * Handle removal.
+		 *
+		 * @param {wp.customize.Control} removedControl Removed control.
+		 * @returns {void}
+		 */
+		handleRemoval: function handleEditorControlRemoval( removedControl ) {
+			var control = this, editor, section;
+			if ( control !== removedControl ) {
+				return;
+			}
+			section = api.section( control.section() );
+			api.control.unbind( 'remove', control.handleRemoval );
+			control.expanded.set( false );
+			control.container.remove();
+
+			editor = tinyMCE.get( 'customize-posts-content' );
+			if ( editor ) {
+				editor.off( 'input change keyup', control.onVisualEditorChange );
+			}
+			control.contentTextarea.off( 'input', control.onTextEditorChange );
+			if ( control.setting ) {
+				control.setting.unbind( control.updateEditorContentsOnSettingChange );
+			}
+			if ( section ) {
+				section.expanded.unbind( control.handleSectionExpansionToggled );
+			}
+			control.editorToggleExpandButton.off( 'click', control.handleToggleEditorButtonClick );
+			control.editorDragbar.off( 'mousedown', control.onMousedownEditorDragbar );
+			control.editorDragbar.off( 'mouseup', control.onMouseupEditorDragbar );
+			$( window ).off( 'resize', control.resizeEditorWithWindowResize );
 		},
 
 		/**
@@ -74,7 +122,7 @@
 		/**
 		 * Expand or collapse control.
 		 *
-		 * @param {boolean}  [expanded] - If not supplied, will be inverse of current visibility
+		 * @param {boolean}[expanded] - If not supplied, will be inverse of current visibility
 		 * @param {Object}   [params] - Optional params.
 		 * @param {Function} [params.completeCallback] - Function to call when the form toggle has finished animating.
 		 * @returns {void}
@@ -188,108 +236,141 @@
 				throw new Error( 'Only one setting may be associated with a post editor control.' );
 			}
 
-			control.editorHeading    = $( '#customize-posts-content-editor-title' );
-			control.contentTextarea  = $( '#customize-posts-content' );
+			control.editorHeading = $( '#customize-posts-content-editor-title' );
+			control.contentTextarea = $( '#customize-posts-content' );
 			control.customizePreview = $( '#customize-preview' );
-			control.editorDragbar    = $( '#customize-posts-content-editor-dragbar' );
-			control.editorPane       = $( '#customize-posts-content-editor-pane' );
-			control.editorFrame      = $( '#customize-posts-content_ifr' );
-			control.collapseSidebar  = $( '.collapse-sidebar' );
-
+			control.editorDragbar = $( '#customize-posts-content-editor-dragbar' );
+			control.editorPane = $( '#customize-posts-content-editor-pane' );
+			control.editorFrame = $( '#customize-posts-content_ifr' );
+			control.collapseSidebar = $( '.collapse-sidebar' );
 			control.editorToggleExpandButton = control.container.find( '.toggle-post-editor' );
+
 			control.updateEditorToggleExpandButtonLabel( control.expanded.get() );
-			control.onTextEditorChange = _.bind( control.onTextEditorChange, control );
-			control.onVisualEditorChange = _.bind( control.onVisualEditorChange, control );
 
-			/**
-			 * Update the editor when the setting changes its state.
-			 */
-			setting.bind( function( newPostData, oldPostData ) {
-				var editor,
-					newData = control.params.setting_property ? newPostData[ control.params.setting_property ] : newPostData,
-					oldData = control.params.setting_property ? oldPostData[ control.params.setting_property ] : oldPostData;
+			setting.bind( control.updateEditorContentsOnSettingChange );
+			section.expanded.bind( control.handleSectionExpansionToggled );
+			control.editorToggleExpandButton.on( 'click', control.handleToggleEditorButtonClick );
+			control.editorDragbar.on( 'mousedown', control.onMousedownEditorDragbar );
+			control.editorDragbar.on( 'mouseup', control.onMouseupEditorDragbar );
+			$( window ).on( 'resize', control.resizeEditorWithWindowResize );
+		},
 
-				if ( control.expanded.get() && ! control.editorSyncSuspended && newData !== oldData ) {
-					control.editorSyncSuspended = true;
-					editor = tinyMCE.get( 'customize-posts-content' );
-					if ( editor && ! editor.isHidden() ) {
-						editor.setContent( wp.editor.autop( newData ) );
-					} else {
-						control.contentTextarea.val( newData );
-					}
-					control.editorSyncSuspended = false;
-				}
-			} );
+		/**
+		 * Update the editor when the setting changes its state.
+		 *
+		 * @param {string|object} newValue New setting value.
+		 * @param {string|object} oldValue Old setting value.
+		 * @returns {void}
+		 */
+		updateEditorContentsOnSettingChange: function updateEditorContentsOnSettingChange( newValue, oldValue ) {
+			var editor,
+				control = this,
+				newData = control.params.setting_property ? newValue[ control.params.setting_property ] : newValue,
+				oldData = control.params.setting_property ? oldValue[ control.params.setting_property ] : oldValue;
 
-			/**
-			 * Unlink the editor from this post and collapse the editor when the section is collapsed.
-			 */
-			section.expanded.bind( function( expanded ) {
-				if ( expanded ) {
-					api.Posts.postIdInput.val( section.params.post_id || false );
+			if ( control.expanded.get() && ! control.editorSyncSuspended && newData !== oldData ) {
+				control.editorSyncSuspended = true;
+				editor = tinyMCE.get( 'customize-posts-content' );
+				if ( editor && ! editor.isHidden() ) {
+					editor.setContent( wp.editor.autop( newData ) );
 				} else {
-					api.Posts.postIdInput.val( '' );
-					control.expanded.set( false );
+					control.contentTextarea.val( newData );
 				}
-			} );
+				control.editorSyncSuspended = false;
+			}
+		},
 
-			/**
-			 * Toggle the editor when clicking the button, focusing on it if it is expanded.
-			 */
-			control.editorToggleExpandButton.on( 'click', function() {
-				var editor = tinyMCE.get( 'customize-posts-content' );
-				control.expanded.set( ! control.expanded() );
-				if ( control.expanded() ) {
-					if ( editor ) {
-						editor.focus();
-					} else {
-						control.contentTextarea.focus();
-					}
+		/**
+		 * Unlink the editor from this post and collapse the editor when the section is collapsed.
+		 *
+		 * @param {boolean} expanded Expanded.
+		 * @returns {void}
+		 */
+		handleSectionExpansionToggled: function handleSectionExpansionToggled( expanded ) {
+			var control = this,
+				section = api.section( control.section() );
+
+			if ( section && expanded ) {
+				api.Posts.postIdInput.val( section.params.post_id || false );
+			} else {
+				api.Posts.postIdInput.val( '' );
+				control.expanded.set( false );
+			}
+		},
+
+		/**
+		 * Toggle the editor when clicking the button, focusing on it if it is expanded.
+		 *
+		 * @returns {void}
+		 */
+		handleToggleEditorButtonClick: function handleToggleEditorButtonClick() {
+			var control = this,
+				editor = tinyMCE.get( 'customize-posts-content' );
+			control.expanded.set( ! control.expanded() );
+			if ( control.expanded() ) {
+				if ( editor ) {
+					editor.focus();
+				} else {
+					control.contentTextarea.focus();
 				}
+			}
+		},
+
+		/**
+		 * Start resizing the editor when the dragbar starts dragging.
+		 *
+		 * @returns {void}
+		 */
+		onMousedownEditorDragbar: function onMousedownEditorDragbar() {
+			var control = this;
+
+			// Note this could also be accomplished by removing the event handler.
+			if ( ! control.expanded() ) {
+				return;
+			}
+
+			$( document ).on( 'mousemove.customize-posts-editor', function( event ) {
+				event.preventDefault();
+				$( document.body ).addClass( 'customize-posts-content-editor-pane-resize' );
+				control.editorFrame.css( 'pointer-events', 'none' );
+				control.resizeEditor( event.pageY );
 			} );
+		},
 
-			// Resize the editor.
-			control.editorDragbar.on( 'mousedown', function() {
+		/**
+		 * Stop resizing the editor when the dragbar starts dragging.
+		 *
+		 * @returns {void}
+		 */
+		onMouseupEditorDragbar: function onMouseupEditorDragbar() {
+			var control  = this;
 
-				// Note this could also be accomplished by removing the event handler.
-				if ( ! control.expanded() ) {
-					return;
-				}
+			// Note this could also be accomplished by removing the event handler.
+			if ( ! control.expanded() ) {
+				return;
+			}
 
-				$( document ).on( 'mousemove.customize-posts-editor', function( event ) {
-					event.preventDefault();
-					$( document.body ).addClass( 'customize-posts-content-editor-pane-resize' );
-					control.editorFrame.css( 'pointer-events', 'none' );
-					control.resizeEditor( event.pageY );
-				} );
-			} );
+			$( document ).off( 'mousemove.customize-posts-editor' );
+			$( document.body ).removeClass( 'customize-posts-content-editor-pane-resize' );
+			control.editorFrame.css( 'pointer-events', '' );
+		},
 
-			// Remove editor resize.
-			control.editorDragbar.on( 'mouseup', function() {
+		/**
+		 * Resize the editor when the viewport changes.
+		 *
+		 * @returns {void}
+		 */
+		resizeEditorWithWindowResize: function resizeEditorWithWindowResize() {
+			var control = this, resizeDelay = 50;
 
-				// Note this could also be accomplished by removing the event handler.
-				if ( ! control.expanded() ) {
-					return;
-				}
+			// Note this could also be accomplished by removing the event handler.
+			if ( ! control.expanded() ) {
+				return;
+			}
 
-				$( document ).off( 'mousemove.customize-posts-editor' );
-				$( document.body ).removeClass( 'customize-posts-content-editor-pane-resize' );
-				control.editorFrame.css( 'pointer-events', '' );
-			} );
-
-			// Resize the editor when the viewport changes.
-			$( window ).on( 'resize', function() {
-				var resizeDelay = 50;
-
-				// Note this could also be accomplished by removing the event handler.
-				if ( ! control.expanded() ) {
-					return;
-				}
-
-				_.delay( function() {
-					control.resizeEditor( window.innerHeight - control.editorPane.height() );
-				}, resizeDelay );
-			} );
+			_.delay( function() {
+				control.resizeEditor( window.innerHeight - control.editorPane.height() );
+			}, resizeDelay );
 		},
 
 		/**
