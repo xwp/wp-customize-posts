ff79c289d670ac67a870ef7bf6ab3c188aa66c8e
Weston Ruter <weston@xwp.co>
2016-08-23 13:59:22 -0700

    Fix theme compat for twentysixteen content rendering by including additional template tags

--- php/theme-support/class-customize-posts-twenty-sixteen-support.php
+++ php/theme-support/class-customize-posts-twenty-sixteen-support.php
@@ -45,6 +45,7 @@ class Customize_Posts_Twenty_Sixteen_Support extends Customize_Posts_Theme_Suppo
 			'container_inclusive' => true,
 			'render_callback' => array( $this, 'biography_render_callback' ),
 		);
+		$schema['post_content']['render_callback'] = array( $this, 'content_render_callback' );
 
 		return $schema;
 	}
@@ -69,5 +70,41 @@ class Customize_Posts_Twenty_Sixteen_Support extends Customize_Posts_Theme_Suppo
 		}
 
 		return $rendered;
+	}
+
+	/**
+	 * Output the additional template tags that the Twenty Sixteen uses in entry-content aside from just the_content().
+	 *
+	 * @link https://github.com/WordPress/twentysixteen/blob/7e5c8d2e966ab3737b2ef411c0b8db0d6a8c57ec/template-parts/content-single.php#L21-L36
+	 * @link https://github.com/WordPress/twentysixteen/blob/7e5c8d2e966ab3737b2ef411c0b8db0d6a8c57ec/template-parts/content-page.php#L19-L30
+	 *
+	 * @param WP_Customize_Partial $partial Partial.
+	 */
+	public function content_render_callback( WP_Customize_Partial $partial ) {
+		global $post;
+
+		if ( ! ( $partial instanceof WP_Customize_Post_Field_Partial ) ) {
+			return;
+		}
+
+		$post = get_post( $partial->post_id ); // WPCS: override global ok.
+		setup_postdata( $post );
+
+		the_content();
+
+		wp_link_pages( array(
+			'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+			'after'       => '</div>',
+			'link_before' => '<span>',
+			'link_after'  => '</span>',
+			'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+			'separator'   => '<span class="screen-reader-text">, </span>',
+		) );
+
+		if ( ! is_page() && '' !== get_the_author_meta( 'description' ) ) {
+			get_template_part( 'template-parts/biography' );
+		}
+
+		wp_reset_postdata();
 	}
 }
