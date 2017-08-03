52c37ce9e6c1e83d561d7b44a31a11e32e3599cb
Weston Ruter <weston@xwp.co>
2017-08-03 16:35:33 -0700

    Add fix for nested edit shortcuts in Twenty Seventeen front-page panels

--- php/theme-support/class-customize-posts-twenty-seventeen-support.php
+++ php/theme-support/class-customize-posts-twenty-seventeen-support.php
@@ -28,6 +28,7 @@ class Customize_Posts_Twenty_Seventeen_Support extends Customize_Posts_Theme_Sup
 	 */
 	public function add_support() {
 		add_filter( 'customize_posts_partial_schema', array( $this, 'filter_partial_schema' ) );
+		add_action( 'wp_head', array( $this, 'print_preview_style' ), 200 );
 	}
 
 	/**
@@ -41,5 +42,35 @@ class Customize_Posts_Twenty_Seventeen_Support extends Customize_Posts_Theme_Sup
 	public function filter_partial_schema( $schema ) {
 		$schema['post_excerpt']['fallback_refresh'] = false; // Not needed because there are no has_excerpt() checks in the theme.
 		return $schema;
+	}
+
+	/**
+	 * Print style for preview.
+	 *
+	 * Due to lack of child selector in `.twentyseventeen-panel .customize-partial-edit-shortcut button`.
+	 * This won't be needed once WP Core #41557 is merged.
+	 *
+	 * @link https://core.trac.wordpress.org/ticket/41557
+	 * @link https://github.com/WordPress/wordpress-develop/blob/4.7.0/src/wp-content/themes/twentyseventeen/style.css#L3043-L3047
+	 */
+	public function print_preview_style() {
+		if ( ! is_customize_preview() ) {
+			return;
+		}
+		?>
+		<style>
+		.widget .customize-partial-edit-shortcut button,
+		.customize-partial-edit-shortcut button {
+			left: -30px !important;
+			top: 2px !important;
+		}
+
+		/* Add some space around the visual edit shortcut buttons. */
+		.twentyseventeen-panel > .customize-partial-edit-shortcut > button {
+			top: 30px !important;
+			left: 30px !important;
+		}
+		</style>
+		<?php
 	}
 }
